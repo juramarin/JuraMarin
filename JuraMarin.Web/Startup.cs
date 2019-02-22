a24b9848fa6db60b124f97f52941bcf9adda6870
juramarin <marin.juranko@gmail.com>
2019-02-23 00:17:17 +0100

    Add Serilog MSSqlServer & Application Insights

--- JuraMarin.Web/Startup.cs
+++ JuraMarin.Web/Startup.cs
@@ -36,7 +36,7 @@ namespace JuraMarin.Web
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+                    Configuration.GetConnectionString("mjDb")));
             services.AddDefaultIdentity<IdentityUser>()
                 .AddEntityFrameworkStores<ApplicationDbContext>();
 
